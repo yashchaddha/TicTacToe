2677241f73835de4cfc8ed7c78380edc0993a5d6
yash <yash@Development.myguest.virtualbox.org>
2023-08-12 02:17:38 +0530

    Passing State to Parent Component and then passing it to child

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import './Square.css'
 
-function Square({value}){
+function Square({value,onSquareClick}){
+
     return (
-        <button className="square">{value}</button>
+        <button className="square" onClick={onSquareClick}>{value}</button>
     );
 }
 
