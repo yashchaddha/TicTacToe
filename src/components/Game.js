440944656d70e00b343c54a3b17cf7184e7f0b59
yashchaddha <yash@Development.myguest.virtualbox.org>
2023-08-13 23:00:47 +0530

    Changes

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,22 @@
 import React from "react";
 import Board from "./Board";
+import './Game.css';
 
 function Game(props){
     return(
-        <Board></Board>
+        <div className="game">
+            <div className="game-board">
+                <Board/>
+            </div>
+
+            <div className="game-info">
+                <ol>
+                    <li>Hi</li>
+                    <li>There</li>
+
+                </ol>
+            </div>
+        </div>
     );
 }
 
